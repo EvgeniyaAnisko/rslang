6ec19a1766e7054a907e016798767ee720856ef2
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-08-18 19:17:40 +0300

    feat: add basic configuration files

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+// const CopyPlugin = require("copy-webpack-plugin");
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                loader: 'ts-loader',
+              },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/i, 
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist/rslang'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*',
+                '!.git',
+            ],
+        }),
+        new EslingPlugin({ 
+            extensions: 
+                'ts' 
+        }),
+        // new CopyPlugin({
+        //     patterns: [
+        //       { from: "src/assets/**/*",
+        //       to: "./",
+        //       force: true,
+        //     },
+        //     ],
+        //     options: {
+        //       concurrency: 100,
+        //     },
+        //   }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
