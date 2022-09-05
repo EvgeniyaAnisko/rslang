5ab75b96ee36488db30daf22d5829961c25078fa
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2022-09-05 21:52:45 +0300

    upate

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-console.log('Hello, RS Lang');
-
-fetch('https://evgeniyaanisko-rslang.herokuapp.com/words?page=2&group=0')
-  .then((res: Response) => res.json())
-  .then((data) => console.log(data))
-  .catch((err: Error) => console.log(err));
